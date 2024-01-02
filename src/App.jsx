36368b1214a7f73f97f2676cc3b07e9657b91bcc
Tehseem110 <Tehseem110>
2024-01-02 22:42:31 +0530

    First Day Typing Compitition

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import TypingTest from './components/TypingTest/TypingTest';
+
+function App() {
+  return (
+    <div className=" h-screen w-screen ">
+      <TypingTest />
+    </div>
+  );
+}
+
+export default App;
