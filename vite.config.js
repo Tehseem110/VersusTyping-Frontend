8fdbff8f28bd469c9523d1920dadf6da01da8a6a
Tehseem110 <Tehseem110>
2024-01-07 19:05:58 +0530

    fixed netlify reouting issue

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+// vite.config.js
 
-// https://vitejs.dev/config/
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5000,
   },
+  base: '/', // Add the base option to specify the base URL
 });
